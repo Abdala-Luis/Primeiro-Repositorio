b8037e2857ae703150a2702b29b72b29fdd6e43c
Abdala A. Luis <abdalaluis16@gmail.com>
2025-09-04 20:53:32 +0200

    Meu primeiro commit

---
+++ Condicoes_075011.js
@@ -0,0 +1,8 @@
+var vel = 345
+console.log(`O seu carro esta andar por ${vel}km/h`)
+
+if(vel >= 150){
+console.log('E isso continua assim')
+}else{
+console.log(`Dirija como um piloto de curida`)
+}
